9a67396ec6e6276182e63d72648b2f921c5bc536
taureau <vixidev99@protonmail.com>
2022-01-05 15:15:43 +0530

    organize jsons

--- markets.js
+++ markets.js
@@ -4,6 +4,9 @@ const markets = {
         "ETH-USDT": "gc5YxJq6KJ1Ks2daH1iLPm4Roo7o-PAzQhr0BahAhmE",
         "ETH-USDC": "Trz9R2BPwQkXhF_NRMsKv2reX_K3-0sE6tXiH8apz_4",
         "UNI-USDC": "kbv1tA3R5LXMlnOipHUpczMiJ-AGzHWtEDzuYmQvCmc"
+    },
+    1000: {
+        "ETH-USDC": "Trz9R2BPwQkXhF_NRMsKv2reX_K3-0sE6tXiH8apz_4",
     }
 }
 export default markets;
