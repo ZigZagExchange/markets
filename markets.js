9ec69bab90770a174a6401ca654f4a624053ac9d
taureau <vixidev99@protonmail.com>
2022-01-09 14:29:56 +0530

    more markets

--- markets.js
+++ markets.js
@@ -3,7 +3,11 @@ const markets = {
     1: {
         "ETH-USDT": "gc5YxJq6KJ1Ks2daH1iLPm4Roo7o-PAzQhr0BahAhmE",
         "ETH-USDC": "Trz9R2BPwQkXhF_NRMsKv2reX_K3-0sE6tXiH8apz_4",
-        "UNI-USDC": "kbv1tA3R5LXMlnOipHUpczMiJ-AGzHWtEDzuYmQvCmc"
+        "UNI-USDC": "kbv1tA3R5LXMlnOipHUpczMiJ-AGzHWtEDzuYmQvCmc",
+        "DAI-USDC": "_Rcroy56mn9HcJOMggq8iRPgdQOxw_Y6JAoYHQ-xm8w",
+        "DAI-USDT": "itjhj-Rjh0l7W4pQsoYZdbwJocJzGl2OpPEf4Cxbz5mY",
+        "ETH-DAI": "B2j7NkWft-N3hMhBv2MBZosqNvMyOTfNmob5fLvCTsM",
+        "USDC-USDT": "xhL4owDmQoEWQMpYYthRKR1aMb5SA8ByOuhG8YRZHe4",
     },
     1000: {
         "ETH-USDC": "9a2HIkOMtEp-DshgwCkz3gKfyp-1T-sddpUAjM8bdPY",
